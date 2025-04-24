677e636b417ef84becccf2c7cc35bf660350a5e3
YangYunjia <yyj980401@126.com>
2025-04-24 04:17:51 -0400

    add 2d integration, change to cell center

--- cfdpost/utils.py
+++ cfdpost/utils.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 from scipy.interpolate import interp1d, splev, splrep
-# from scipy.optimize import leastsq
+from scipy.optimize import leastsq
 
 
 class Fitting():
@@ -71,22 +71,27 @@ def _xy_2_cl(dfp: np.ndarray, aoa: float) -> np.ndarray:
 def get_dxyforce_1d(geom: np.ndarray, cp: np.ndarray, cf: np.ndarray=None) -> np.ndarray:
     '''
     integrate the force on each surface grid cell
-
+    ### input
+    cp & cf in the cell center
+    
     ### retrun
     np.ndarray (N-1, 2) 
         (dFx, dFy)
     
     '''
 
-    dfp_n  = (0.5 * (cp[1:] + cp[:-1])).reshape((1, -1))
-    if cf is None:
+    dr     = (geom[1:] - geom[:-1])
+    dfp_n  = cp.reshape((1, -1))
+    if cf is None or len(cf.shape) > 1:
         dfv_t  = np.zeros_like(dfp_n)
     else:
-        dfv_t = (0.5 * (cf[1:] + cf[:-1])).reshape((1, -1))
+        dfv_t = cf.reshape((1, -1))
 
-    dr     = (geom[1:] - geom[:-1])
+    dxy = np.einsum('lj,lpk,jk->jp', np.concatenate((dfv_t, -dfp_n), axis=0), _rot_metrix, dr)
 
-    return np.einsum('lj,lpk,jk->jp', np.concatenate((dfv_t, -dfp_n), axis=0), _rot_metrix, dr)
+    if len(cf.shape) > 1: dxy += np.abs(dr) * cf
+    
+    return dxy
 
 
 def get_xyforce_1d(geom: np.ndarray, cp: np.ndarray, cf: np.ndarray=None) -> np.ndarray:
@@ -129,6 +134,7 @@ def get_force_1d(geom: np.ndarray, aoa: float, cp: np.ndarray, cf: np.ndarray=No
     `profile`: The pressure profile, shape: (N, ); should be non_dimensional pressure profile by freestream condtion
 
         Cp = (p - p_inf) / 0.5 * rho * U^2
+        Cf = tau / 0.5 * rho * U^2
     
     `aoa`:  angle of attack
 
@@ -139,6 +145,103 @@ def get_force_1d(geom: np.ndarray, aoa: float, cp: np.ndarray, cf: np.ndarray=No
     dfp = get_xyforce_1d(geom, cp, cf)
     return _xy_2_cl(dfp, aoa)
 
+def get_cellinfo_2d(geom: np.ndarray) -> np.ndarray:
+    
+    '''
+    params:
+    ===
+    `geom`:  The geometry (x, y, z), shape: (I, J, 3)
+
+    returns:
+    ===
+    `normals`: shape: (I-1, J-1, 3)
+    `areas`  : shape: (I-1, J-1)
+    '''
+    
+    # get corner points（p0, p1, p2, p3）
+    p0 = geom[:-1, :-1] # SW
+    p1 = geom[:-1, 1:]  # SE
+    p2 = geom[1:, 1:]   # NW
+    p3 = geom[1:, :-1]  # NE
+
+    # calculate two groups of normal vector and average
+    normals = 0.5 * (np.cross(p1 - p0, p3 - p0) + np.cross(p2 - p1, p0 - p1))
+    areas   = 0.5 * (np.linalg.norm(np.cross(p1 - p0, p2 - p0), axis=-1) + np.linalg.norm(np.cross(p2 - p0, p3 - p0), axis=-1))
+
+    # normalization
+    normals /= (np.linalg.norm(normals, axis=-1, keepdims=True) + 1e-20)
+    return normals, areas    
+
+def get_dxyforce_2d(geom: np.ndarray, cp: np.ndarray, cf: np.ndarray=None) -> np.ndarray:
+    '''
+        
+    return:
+    ===
+    np.ndarray: (CX, CY, CZ), shape (I, J, 3)
+    
+    '''
+    # calculate normal vector
+    n, a = get_cellinfo_2d(geom)
+    dfp = cp[..., np.newaxis] * n * a[..., np.newaxis]
+    
+    if cf is not None:
+        dfp += (cf - np.sum(cf * n, axis=-1, keepdims=True) * n) * a[..., np.newaxis]
+    
+    return dfp
+
+def get_xyforce_2d(geom: np.ndarray, cp: np.ndarray, cf: np.ndarray=None) -> np.ndarray:
+    '''
+    
+    return:
+    ===
+    np.ndarray: (CX, CY, CZ)
+    '''
+    return np.sum(get_dxyforce_2d(geom, cp, cf), axis=(0,1))
+
+def get_force_2d(geom: np.ndarray, aoa: float, cp: np.ndarray, cf: np.ndarray=None) -> np.ndarray:
+    '''
+    integrate lift and drag from 2D surface data
+    
+    params:
+    ===
+    `geom`:  The geometry (x, y, z), shape: (I, J, 3)
+    `aoa`: in DEGREE
+    `cp`  :  shape: (I-1, J-1)
+         Cp = (p - p_inf) / 0.5 * rho * U^2
+    `cf`:    (Cfx, Cfy, Cfz), shape: (I-1, J-1, 3)
+         Cf = (tau @ n) / 0.5 * rho * U^2 
+         
+        
+    return:
+    ===
+    np.ndarray: (CD, CL, CZ)
+    '''
+    dfp = get_xyforce_2d(geom, cp, cf)
+    dfp[:2] = _xy_2_cl(dfp[:2], aoa)
+    return dfp
+
+def get_moment_2d(geom: np.ndarray, cp: np.ndarray, cf: np.ndarray=None, ref_point: np.ndarray=np.array([0.25, 0, 0])) -> np.ndarray:
+    '''
+    integrate moment from 2D surface data
+    
+    params:
+    ===
+    `geom`:  The geometry (x, y, z), shape: (I, J, 3)
+    `cp`  :  shape: (I-1, J-1)
+         Cp = (p - p_inf) / 0.5 * rho * U^2
+    `cf`:    (Cfx, Cfy, Cfz), shape: (I-1, J-1, 3)
+         Cf = (tau @ n) / 0.5 * rho * U^2 
+    `ref_point`: np.ndarray: shape: (3,)
+        
+    return:
+    ===
+    np.ndarray: (CMx, CMy, CMz)
+    '''
+    
+    dxyforce = get_dxyforce_2d(geom, cp, cf)
+    r = 0.25 * (geom[:-1, :-1] + geom[:-1, 1:] + geom[1:, 1:] + geom[1:, :-1]) - ref_point
+    
+    return np.sum(np.cross(r, dxyforce), axis=(0, 1))
 
 def clustcos(i: int, nn: int, a0=0.0079, a1=0.96, beta=1.0) -> float:
     '''
