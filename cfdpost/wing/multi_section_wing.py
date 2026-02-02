c9fb313a4f3f3912a2e1a1aaf56e9e7a338bfa5a
Yunjia Yang <yyj980401@126.com>
2026-02-02 04:17:42 -0500

    update multi_section_wing for opt

--- cfdpost/wing/multi_section_wing.py
+++ cfdpost/wing/multi_section_wing.py
@@ -89,7 +89,8 @@ class MultiSecWing(BasicParaWing):
             g['half_span'] = 0.25 * g['AR'] * (1 + g['TR'])
 
     @classmethod
-    def _reconstruct_surface_grids(cls, g: dict, nx: int, nzs: List[int], tail: float=0.008) -> np.ndarray:
+    def _reconstruct_surface_grids(cls, g: dict, nx: int, nzs: List[int], tail: float=0.008, 
+                                   zaxis=0., lower_cst_constraints: bool = False, twists0: float = 0.) -> np.ndarray:
         '''
         This function reconstructs reference mesh (grid points) of wings for ML models
 
@@ -99,28 +100,39 @@ class MultiSecWing(BasicParaWing):
         - There's no mesh points on the tailing edge
 
         The others are the same of `~/mount/baselinewings/f6-1/gen-mesh.py`
-        
+
+        At 30.1.2026, we compare the code here with crm_ml_twist/mesh.py and make them are the same
+
+        param of `g`:
+        ===
+
+        - for WebWing
+
+            `SA`, `AR`, `TR`, `kink`, `rootadj`, `tmaxs`(7), `DAs`(7), `twists`(7), `cst_u`(7x10), `cst_l`(7x10)
+
+        - for ML opt
+
+            `half_span`, `SA`, `kink`, `chords`(3), `DAs`(7), `twists`(6), `root_twist`, `cst_u`(70), `cst_l`(70 / 63)
+
         '''
+
+        print('!!!!! building ML mesh')
         
         # get distributed values
         cabin_ratio = 0.1
-        TR = g['TR']
         half_span = g['half_span'] 
-        kink = half_span * g['kink']
         cabin = half_span * cabin_ratio
         
         # get spanwise eta distribution (evenly distributed)
         yLEs = np.linspace(cabin, half_span, nzs[0])
-        i_kink = math.floor((g['kink']-cabin_ratio)/(1-cabin_ratio) * nzs[0])
-        yLEs0 = yLEs[:i_kink+1]
-        yLEs1 = yLEs[i_kink+1:]
 
         # leading edge parameters
         tip = half_span + 1e-9
 
         if 'control_points' not in g.keys():
+            kink = half_span * g['kink']
             if 'DAs' in g.keys():
-                _control_points = np.concatenate((np.linspace(cabin, kink, 3), np.linspace(kink, half_span, 5)[1:]), axis=0)
+                _control_points = list(np.concatenate((np.linspace(cabin, kink, 3), np.linspace(kink, half_span, 5)[1:]), axis=0))
                 _control_points_mid = _control_points
             else:
                 mid_outer = 0.5 * (kink + half_span)
@@ -128,6 +140,7 @@ class MultiSecWing(BasicParaWing):
                 _control_points = [0, kink, tip]
                 _control_points_mid = [0, mid_inner, kink, mid_outer, tip]
         else:
+            kink = half_span * g['control_points'][2]
             _control_points = [half_span * eta for eta in g['control_points']]
             _control_points_mid = _control_points
         
@@ -138,27 +151,43 @@ class MultiSecWing(BasicParaWing):
 
         ### ****
         ### dihedral angle
-        if 'DA_kink' in g.keys():
+        if 'DA_kink' in g.keys() and 'DA' in g.keys():
+
+            i_kink = math.floor((g['kink']-cabin_ratio)/(1-cabin_ratio) * nzs[0])
+            yLEs0 = yLEs[:i_kink+1]
+            yLEs1 = yLEs[i_kink+1:]
+
             zLEs0 = np.tan(g['DA_kink']/DEGREE) * yLEs0  # inner section, linear distribution
             zLE_kink = np.tan(g['DA_kink']/DEGREE) * kink
             zLE_tip = np.tan(g['DA']/DEGREE) * half_span # zLE at kink
             zLE_cs = CubicSpline([kink, tip], [zLE_kink, zLE_tip], bc_type=((1, np.tan(g['DA_kink']/DEGREE)), 'not-a-knot'), extrapolate=False)
             zLEs1 = zLE_cs(yLEs1)
             zLEs  = np.concatenate((zLEs0, zLEs1), axis=0)
-        else:
-            zLE_cs = CubicSpline(_control_points, [reduce(lambda x, y: x + y, g['DAs'][:i+1]) for i in range(len(g['DAs']))], extrapolate=False)
+
+        elif 'DAs' in g.keys():
+            zLE_cs = CubicSpline([0] + _control_points, [0] + [reduce(lambda x, y: x + y, g['DAs'][:i+1]) for i in range(len(g['DAs']))], extrapolate=False)
             zLEs = zLE_cs(yLEs)
+
+        else:
+            raise AttributeError()
 
         ### ****
         ### chord
-        chord_tip   = TR
-        chord_kink  = TR * g['kink'] + 1 * (1 - g['kink'])
-        root_adj_l  = g['rootadj'] * (xLE_kink + chord_kink - 1)
-        chord_cabin = TR * cabin_ratio + 1 * (1 - cabin_ratio) + root_adj_l * (g['kink'] - cabin_ratio) / g['kink']
-        chord_root = 1 + root_adj_l 
-        chords0 = chord_cabin + (chord_kink - chord_cabin) * (yLEs0 - cabin) / (kink - cabin)
-        chords1 = chord_kink + (chord_tip  - chord_kink) * (yLEs1 - kink) / (half_span - kink)
-        chords  = np.concatenate((chords0, chords1), axis=0)
+        if 'chords' in g.keys():
+            chord_cabin, chord_kink, chord_tip = g["chords"]
+        else:
+            TR = g['TR']
+            chord_tip   = TR
+            chord_kink  = TR * g['kink'] + 1 * (1 - g['kink'])
+            root_adj_l  = g['rootadj'] * (xLE_kink + chord_kink - 1)
+            chord_cabin = TR * cabin_ratio + 1 * (1 - cabin_ratio) + root_adj_l * (g['kink'] - cabin_ratio) / g['kink']
+            chord_root = 1 + root_adj_l 
+
+        chords = np.where(
+            yLEs < kink,
+            chord_cabin + (chord_kink - chord_cabin) * (yLEs - cabin) / (kink - cabin),
+            chord_kink + (chord_tip - chord_kink) * (yLEs - kink) / (half_span - kink)
+        )
 
         g['surface_area'] = (chord_cabin + chord_kink) * (kink - cabin) * 0.5 + (chord_kink + chord_tip) * (half_span - kink) * 0.5
         
@@ -185,15 +214,20 @@ class MultiSecWing(BasicParaWing):
         else:
             # reconstruct airfoils with CSTs and store them in zzss
             zzss = []
+            cst_u = np.array(g['cst_u']).reshape(len(_control_points), -1)
+            cst_l = np.array(g['cst_l']).reshape(len(_control_points), -1)
+
+            if lower_cst_constraints and cst_u.shape[1] == cst_l.shape[1] + 1:
+                cst_l = np.concatenate((-cst_u[:, [0]], cst_l), axis=1)
+
             for i in range(len(_control_points)):
-                xx1, yus, yls, _, _ = cst_foil(nn=nx, cst_u=g['cst_u'][i], cst_l=g['cst_l'][i], t=g['tmaxs'][i] if 'tmaxs' in g.keys() else None, tail=tail)
+                xx1, yus, yls, _, _ = cst_foil(nn=nx, cst_u=cst_u[i], cst_l=cst_l[i], t=g['tmaxs'][i] if 'tmaxs' in g.keys() else None, tail=tail)
                 zzss.append(np.concatenate((np.flip(yls[1:], axis=0), yus), axis=0))
             
             zzs = []
             for yy in yLEs:
                 if yy < _control_points[0]:
                     zzs.append(zzss[0] + (yy - _control_points[0]) / (_control_points[1] - _control_points[0]) * (zzss[1] - zzss[0]))
-                    print(f'[warning] extrapolate for y = {yy:.2f}')
                 elif yy - _control_points[-1] > 0:
                     raise RuntimeError()
                 else:
@@ -206,9 +240,12 @@ class MultiSecWing(BasicParaWing):
             zzs = np.array(zzs).transpose()
             
         # twist
-        tws_cs = CubicSpline(_control_points_mid, [reduce(lambda x, y: x + y, g['twists'][:i+1]) for i in range(len(g['twists']))], extrapolate=True)
+        twists_angles = ([g['root_twist']] if 'root_twist' in g.keys() else []) + list(g['twists'])
+
+        tws_cs = CubicSpline(_control_points_mid, [reduce(lambda x, y: x + y, twists_angles[:i+1]) + twists0 for i in range(len(twists_angles))], extrapolate=True)
         tws = tws_cs(yLEs)
-        
+        zLEs += zaxis * chords * np.sin(tws/180*np.pi)
+ 
         # rotation
         xxs = np.concatenate((np.flip(xx1[1:], axis=0), xx1), axis=0)
 
@@ -220,8 +257,7 @@ class MultiSecWing(BasicParaWing):
         zzs1 = zzs1 * chords[None, :] + zLEs[None, :]
 
         # print(xxs.shape, np.repeat(yLEs[None, :], 2*nx-1, axis=0).shape, zzs.shape)
-        block = np.stack((xxs1, np.repeat(yLEs[None, :], 2*nx-1, axis=0), zzs1)).transpose(2, 1, 0)[..., [0, 2, 1]]
-        # block[..., 2] = -block[..., 2]
+        block = np.stack((xxs1, zzs1, np.repeat(yLEs[None, :], 2*nx-1, axis=0)), axis=0).transpose(0, 2, 1) # C, Nz, Ni
 
         return block
     
